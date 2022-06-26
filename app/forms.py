5ee2fcff7a4daac111303a17e24ac695d9c7139b
Per Jensen <perjensen@le-bigmac.home>
2022-06-26 20:19:41 +0100

    adjusted subtitles to be more appealing

--- app/forms.py
+++ app/forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class SummarizeForm(FlaskForm):
-    prompt = StringField('What would you like to summarize?', validators=[DataRequired()])
+    prompt = StringField('What Would You Like to Summarize?', validators=[DataRequired()])
     submit = SubmitField('Submit')
